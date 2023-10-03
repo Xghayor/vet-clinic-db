3df95d1b55ca08540c057bcc5a01a798ecca6040
vikas924 <obroy902@gmail.com>
2023-10-03 18:00:59 +0530

    Add indexes

--- schema.sql
+++ schema.sql
@@ -96,3 +96,10 @@ CREATE TABLE visits (
 -- Day 5
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- Create Index for visits (animals_id)
+CREATE INDEX idx_animals_id ON visits (animals_id);
+-- Create Index for visits (animals_id)
+CREATE INDEX idx_vet_id ON visits (vet_id);
+-- Create Index for visits (animals_id)
+CREATE INDEX idx_email ON owners (email);
