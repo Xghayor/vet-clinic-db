499b6aaf9c6c6d9bef1309f54976d2e3b84f1803
Ghayoor Ul Baqir <cc.baqir@gmail.com>
2023-10-03 05:18:26 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,45 +1,93 @@
-/* Database schema to keep the structure of entire database. */
+-- Day 1
 
 CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg FLOAT
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(255),
+  date_of_birth date,
+  escape_attempts integer,
+  neutered boolean,
+  weight_kg decimal
 );
 
+-- Day 2
 
-ALTER TABLE animals ADD species varchar(255);
-
-CREATE TABLE owners (
-     id SERIAL PRIMARY KEY,
-     full_name VARCHAR(100),
-     age INT
-);
-
-CREATE TABLE species (
-    id serial PRIMARY KEY,
-    species_name varchar(255)
-);
-
-ALTER TABLE animals DROP COLUMN species;
-
-ALTER TABLE animals ADD species_id INT;
-ALTER TABLE animals ADD owners_id INT;
-
+-- Add a column species of type string to your animals table. Modify your schema.sql file.
 ALTER TABLE animals
-ADD CONSTRAINT fk_species_id
-FOREIGN KEY (species_id)
-REFERENCES species(id);
+ADD COLUMN species VARCHAR(255);
 
+-- Day 3
+-- Create a table named owners with the following columns:
+-- 	id: integer (set it as autoincremented PRIMARY KEY)
+-- 	full_name: string
+-- 	age: integer
+CREATE TABLE owners(
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	full_name VARCHAR(255), 
+	age INT
+);
+-- DONE
+
+-- Create a table named species with the following columns:
+-- 	id: integer (set it as autoincremented PRIMARY KEY)
+-- 	name: string
+CREATE TABLE species(
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(255)
+);
+-- DONE
+
+-- Modify animals table:
+-- 	Make sure that id is set as autoincremented PRIMARY KEY
+-- Type SERIAL to column id was set immidiately after creating animals table. 
+-- Please see line 12 - 19.
+-- DONE
+
+-- 	Remove column species from animals table:
 ALTER TABLE animals
-ADD CONSTRAINT fk_owners_id
-FOREIGN KEY (owners_id)
-REFERENCES owners(id);
+DROP COLUMN species;
+-- DONE
 
+-- 	Add column species_id into animals table which is a foreign key referencing species table
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id);
+-- DONE
 
+-- 	Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners(id);
+-- DONE
 
+-- Day 4
+-- - Create a table named `vets` with the following columns:
+--   - `id`: integer (set it as autoincremented PRIMARY KEY)
+--   - `name`: string
+--   - `age`: integer
+--   - `date_of_graduation`: date
+BEGIN;
+CREATE TABLE vets (
+	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(255),
+	age INT,
+	date_of_graduation DATE
+);
+COMMIT;
+	
+-- - There is a **many-to-many** relationship between the tables `species` and `vets`: 
+			-- a vet can specialize in multiple species, and 
+			-- a species can have multiple vets specialized in it. 
+			-- Create a "join table" called `specializations` to handle this relationship.
+CREATE TABLE specializations (
+	vet_id INT REFERENCES vets(id),
+	species_id INT REFERENCES species(id)
+);
 
-
-
+-- - There is a **many-to-many** relationship between the tables `animals` and `vets`: 
+			-- an animal can visit multiple vets 
+			-- and one vet can be visited by multiple animals. 
+			-- Create a "join table" called `visits` to handle this relationship, 
+			-- it should also keep track of the date of the visit.
+CREATE TABLE visits (
+  animals_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  visit_date DATE
+);
