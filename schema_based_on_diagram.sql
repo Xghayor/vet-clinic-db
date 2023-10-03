6e1dc0df961a78d9c01e679a5cc1451831e133de
Vikas chib <obroy902@gmail.com>
2023-10-03 20:14:34 +0530

    Add index

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,6 +14,8 @@ CREATE TABLE medical_histories (
   CONSTRAINT patient_id_fk FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
 
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories (patient_id);
+
 -- Create a table for treatments
 CREATE TABLE treatments (
   id SERIAL PRIMARY KEY,
@@ -30,6 +32,9 @@ CREATE TABLE invoices (
   medical_history_id INT,
   CONSTRAINT medical_history_id_fk FOREIGN KEY(medical_history_id) REFERENCES medical_history(id)
 );
+
+CREATE INDEX idx_invoices_medical_history_id ON invoices (medical_history_id);
+
 -- Create a table for invoice_items
 CREATE TABLE invoice_items (
   id SERIAL PRIMARY KEY,
@@ -41,10 +46,18 @@ CREATE TABLE invoice_items (
   CONSTRAINT invoice_id_fk FOREIGN KEY(invoice_id) REFERENCES invoices(id),
   CONSTRAINT treatment_id_fk FOREIGN KEY(treatment_id) REFERENCES treatments(id)
 );
+
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
+
 -- Craete a Many to many table between medical_histories and treatments
-CREATE medical_treatments (
+CREATE TABLE medical_treatments (
   medical_histories_id INT,
   treatments_id INT,
   CONSTRAINT medical_histories_id_fk FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id),
   CONSTRAINT treatment_id_fk FOREIGN KEY(treatments_id) REFERENCES treatments(id)
 );
+
+CREATE INDEX idx_medical_treatments_medical_histories_id ON medical_treatments (medical_histories_id);
+
+CREATE INDEX idx_medical_treatments_treatments_id ON medical_treatments (treatments_id);
